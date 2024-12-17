25d084cff1d8a7815fdff9b9d796c35a6cab042e
Hoarau Kévin <kevin.h8@hotmail.fr>
2024-12-17 16:59:59 +0000

    first-commit

---
+++ src/Controller/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\PasswordHasher\PasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/sign-up', name: 'sign_up')]
+    public function sign(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $user->getPassword();
+
+            $hash = $passwordHasher->hashPassword($user, $password);
+            $user->setPassword($hash)
+                ->setRoles(['ROLE_USER']);
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute('login');
+        }
+        return $this->render('user/sign_up.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
