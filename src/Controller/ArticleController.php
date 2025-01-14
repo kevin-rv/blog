9c840993b4b95edb27516bfc6bba7ea78305c4fe
Kévin Hoarau <kevinhoarau@MBP-de-Kevin.lan>
2025-01-14 00:52:34 +0000

    I add update and delete in ArticleController , i managed image file

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -7,6 +7,8 @@ use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -43,17 +45,110 @@ class ArticleController extends AbstractController
         $form = $this->createForm(ArticleType::class, $article);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $article = $form->getData();
+            $file = $form->get('image')->getData();
+            if ($file) {
+                $filename = uniqid().'.'.$file->guessExtension();
+                try {
+                    $file->move(
+                        $this->getParameter('images_directory'), 
+                        $filename
+                    );
+                } catch (FileException $e) {
+        
+                }
+                $article->setImage($filename);
+            }
+            //$article = $form->getData();
             $article->setUser($this->getUser())
                 ->setCreatedAt(new \DateTimeImmutable())
                 ->setUpdatedAt(new \DateTimeImmutable());
 
             $em->persist($article);
             $em->flush();
+           
             return $this->redirectToRoute('article');
         }
         return $this->render('article/create.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/article/{articleId}/edit', name: 'article_update', requirements: ['articleId' => '\d+'])]
+    public function update(Request $request,ArticleRepository $repository, int $articleId, EntityManagerInterface $em, Security $security): Response
+    {
+        $article = $repository->find($articleId);
+        $userLoginId = $security->getUser()->getId();
+        $userArticleId = $article->getUser()->getId();
+        if ($userLoginId === $userArticleId) {
+            $form = $this->createForm(ArticleType::class, $article);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $file = $form->get('image')->getData();
+            if ($file) 
+            {
+                $oldFileName = $article->getImage();
+                $filename = uniqid().'.'.$file->guessExtension();
+                try 
+                {
+                    $file->move(
+                        $this->getParameter('images_directory'), 
+                        $filename
+                    );
+
+                    if ($oldFileName) {
+                        $oldFilepath = $this->getParameter('images_directory') . '/' . $oldFileName;
+                        if (file_exists($oldFilepath)) {
+                            unlink($oldFilepath); 
+                        }
+                    }
+                    $article->setImage($filename);
+                } 
+                catch (FileException $e) 
+                {
+                    
+                }
+                
+            }
+                $article->setUpdatedAt(new \DateTimeImmutable());
+                $em->persist($article);
+                $em->flush();
+                $this->addFlash("success", "modification réussi");
+                return $this->redirectToRoute('article');
+            }
+            
+            return $this->render('article/update.html.twig', [
+                'article' => $article,
+                'form' => $form->createView(),
+            ]);
+        }
+
+        $this->addFlash("danger", "vous n'êtes pas propriétaire de l'article");
+        return $this->redirectToRoute('article');
+    }
+
+    #[Route('/article/{articleId}/delete', name: 'article_delete', requirements: ['articleId' => '\d+'])]
+    public function delete(ArticleRepository $articleRepository, int $articleId, EntityManagerInterface $em, Security $security): Response
+    {
+        $article = $articleRepository->find($articleId);
+        $userLoginId = $security->getUser()->getId();
+        $userArticleId = $article->getUser()->getId();
+        if ($userLoginId === $userArticleId) {
+            $file = $article->getImage();
+            if($file) {
+
+                $filepath = $this->getParameter('images_directory') . '/' . $file;
+                if (file_exists($filepath)) {
+                    unlink($filepath); 
+                }
+                
+            }
+            $em->remove($article);
+            $em->flush();
+            $this->addFlash("success", "suppression réussi");
+        }
+        else {
+            $this->addFlash("danger", "suppression echouer");
+        }
+        return $this->redirectToRoute('article');
     }
 }
--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -7,6 +7,7 @@ use App\Entity\Category;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -19,7 +20,11 @@ class ArticleType extends AbstractType
         $builder
             ->add('title')
             ->add('content', TextareaType::class)
-            ->add('image')
+            ->add('image', FileType::class, [
+                'label' => 'Image (JPEG, PNG) 2mo max',
+                'required' => false,
+                'mapped' => false
+            ])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'title',
